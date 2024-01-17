f2f94f7ee61825fbd094b10c7e959261d5dd5ce1
Aniruddh Singh <96120907+Aniruddh-Singh@users.noreply.github.com>
2024-01-17 19:58:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Start writing your code from here.
+function addTask() {
+    var input = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+
+    if (input.value.trim() !== "") {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        taskList.appendChild(li);
+    }
+}
